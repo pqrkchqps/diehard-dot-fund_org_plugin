02ad39e241c577075617bb513b0fa8160d46f521
James Kiesel <james.kiesel@gmail.com>
2016-03-08 08:46:28 +1300

    Add pages controller and views

--- plugin.rb
+++ plugin.rb
@@ -6,17 +6,18 @@ module Plugins
 
         plugin.use_component :angular_feedback_card, outlet: 'after_start_menu'
 
-        plugin.use_page :about,                    'pages#about'
-        plugin.use_page :privacy,                  'pages#privacy'
-        plugin.use_page :purpose,                  'pages#purpose'
-        plugin.use_page :services,                 'pages#services'
-        plugin.use_page :terms_of_service,         'pages#terms_of_service'
-        plugin.use_page :third_parties,            'pages#third_parties'
-        plugin.use_page :try_it,                   'pages#try_it'
-        plugin.use_page :wallets,                  'pages#wallets'
-        plugin.use_page :pricing,                  'pages#pricing'
-        plugin.use_page :translation,              'pages#translation'
-        plugin.use_page :crowdfunding_celebration, 'pages#crowdfunding_celebration'
+        LOOMIO_ORG_PAGES = %w(about
+                              browser_not_supported
+                              crowdfunding_celebration
+                              pricing
+                              privacy
+                              purpose
+                              terms_of_service
+                              third_parties
+                              translation
+                              wallets)
+        LOOMIO_ORG_PAGES.each { |page| plugin.use_page page, "pages##{page}" }
+        plugin.use_class 'controllers/pages_controller'
 
         plugin.use_page :blog,          'http://blog.loomio.org',                                redirect: true
         plugin.use_page :press,         'http://blog.loomio.org/press-pack',                     redirect: true
@@ -24,6 +25,7 @@ module Plugins
         plugin.use_page :roadmap,       'https://trello.com/b/tM6QGCLH/loomio-roadmap',          redirect: true
         plugin.use_page :community,     'https://www.loomio.org/g/WmPCB3IR/loomio-community',    redirect: true
         plugin.use_page :timeline,      'http://www.tiki-toki.com/timeline/entry/313361/Loomio', redirect: true
+
       end
     end
   end
