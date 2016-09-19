d497680f8653e23e7d226d8249bfbef36d6415e3
Hannah Salmon <hr.salmon@gmail.com>
2016-09-19 12:15:08 +1200

    Update routes and links to point to gitbook pages

--- plugin.rb
+++ plugin.rb
@@ -9,21 +9,21 @@ module Plugins
                               crowdfunding_celebration
                               marketing
                               pricing
-                              privacy
-                              terms_of_service
-                              third_parties
                               translation
                               wallets)
         LOOMIO_ORG_PAGES.each { |page| plugin.use_page page, "pages##{page}" }
         plugin.use_class 'controllers/pages_controller'
 
-        plugin.use_page :help,          'http://help.loomio.org',                                redirect: true
-        plugin.use_page :blog,          'http://blog.loomio.org',                                redirect: true
-        plugin.use_page :press,         'http://blog.loomio.org/press-pack',                     redirect: true
-        plugin.use_page :"press-pack",  'http://blog.loomio.org/press-pack',                     redirect: true
-        plugin.use_page :roadmap,       'https://trello.com/b/tM6QGCLH/loomio-roadmap',          redirect: true
-        plugin.use_page :community,     'https://www.loomio.org/g/WmPCB3IR/loomio-community',    redirect: true
-        plugin.use_page :timeline,      'http://www.tiki-toki.com/timeline/entry/313361/Loomio', redirect: true
+        plugin.use_page :help,             'http://help.loomio.org',                                                 redirect: true
+        plugin.use_page :blog,             'http://blog.loomio.org',                                                 redirect: true
+        plugin.use_page :press,            'http://blog.loomio.org/press-pack',                                      redirect: true
+        plugin.use_page :"press-pack",     'http://blog.loomio.org/press-pack',                                      redirect: true
+        plugin.use_page :roadmap,          'https://trello.com/b/tM6QGCLH/loomio-roadmap',                           redirect: true
+        plugin.use_page :community,        'https://www.loomio.org/g/WmPCB3IR/loomio-community',                     redirect: true
+        plugin.use_page :timeline,         'http://www.tiki-toki.com/timeline/entry/313361/Loomio',                  redirect: true
+        plugin.use_page :privacy,          'https://loomio.gitbooks.io/manual/content/en/privacy_policy.html',       redirect: true
+        plugin.use_page :terms_of_service, 'https://loomio.gitbooks.io/manual/content/en/terms_of_service.html',     redirect: true
+        plugin.use_page :third_parties,    'https://loomio.gitbooks.io/manual/content/en/third_party_services.html', redirect: true
 
         plugin.extend_class ApplicationHelper do
           def hosted_by_loomio?
