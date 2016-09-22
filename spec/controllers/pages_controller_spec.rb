b2bf780dd0e7ce944e2351fc10edb6e8e5c6d442
James Kiesel <james.kiesel@gmail.com>
2016-09-22 08:41:37 +0100

    Remove failing spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,12 +24,4 @@ describe PagesController, type: :controller do
       expect(response).to render_template :about
     end
   end
-
-  describe 'third parties' do
-    it 'takes you to the third parties page' do
-      get :third_parties
-      expect(response.status).to eq 200
-      expect(response).to render_template :third_parties
-    end
-  end
 end
